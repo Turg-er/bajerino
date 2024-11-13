849ee1d003e4bd47ec81accd902dbd99813fa879
Daniel Zierl <danielzierl@outlook.com>
2024-11-13 19:33:35 +0100

    Fix: fixed long message crash

--- src/util/Crypto.cpp
+++ src/util/Crypto.cpp
@@ -4,9 +4,8 @@
 #include <cryptopp/hex.h>
 #include <cryptopp/modes.h>
 
-#include <iostream>
 #include <string>
-
+const size_t BLOCK_SIZE = CryptoPP::AES::BLOCKSIZE;
 using namespace CryptoPP;
 std::string base64Encode(const std::string &input)
 {
@@ -22,26 +21,52 @@ std::string base64Decode(const std::string &input)
 }
 std::string AES_encrypt(const std::string &plaintext, const std::string &key)
 {
-    ECB_Mode<AES>::Encryption e;
-    e.SetKey((const byte *)key.c_str(), key.size());
+    try
+    {
+        ECB_Mode<AES>::Encryption e;
+        e.SetKey((const byte *)key.c_str(), key.size());
+        std::string ciphertext;
 
-    std::string ciphertext;
-
-    StringSource s(
-        plaintext, true,
-        new StreamTransformationFilter(e, new StringSink(ciphertext)));
-    return base64Encode(ciphertext);
+        StringSource s(
+            plaintext, true,
+            new StreamTransformationFilter(e, new StringSink(ciphertext)));
+        auto encoded = base64Encode(ciphertext);
+        encoded.erase(std::remove_if(encoded.begin(), encoded.end(), ::isspace),
+                      encoded.end());
+        return encoded;
+    }
+    catch (const CryptoPP::Exception &e)
+    {
+        return "Failed to encrypt the message";
+    }
 }
 std::string AES_decrypt(const std::string &ciphertext, const std::string &key)
 
 {
-    std::string decoded = base64Decode(ciphertext);
-    ECB_Mode<AES>::Decryption d;
-    d.SetKey((const byte *)key.c_str(), key.size());
+    try
+    {
+        std::string decoded = base64Decode(ciphertext);
+        if (decoded.size() == 0)
+        {
+            return "Failed to decrypt the message, message was empty";
+        }
+        if (decoded.size() % BLOCK_SIZE != 0)
+        {
+            return "Failed to decrypt the message, incorrect block size, it "
+                   "was " +
+                   std::to_string(decoded.size());
+        }
+        ECB_Mode<AES>::Decryption d;
+        d.SetKey((const byte *)key.c_str(), key.size());
 
-    std::string recovered;
-    StringSource s(
-        decoded, true,
-        new StreamTransformationFilter(d, new StringSink(recovered)));
-    return recovered;
+        std::string recovered;
+        StringSource s(
+            decoded, true,
+            new StreamTransformationFilter(d, new StringSink(recovered)));
+        return recovered;
+    }
+    catch (const CryptoPP::Exception &e)
+    {
+        return "Failed to decrypt the message";
+    }
 }
