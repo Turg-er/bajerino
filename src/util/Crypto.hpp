d73b02990b96e661008edf7bb992d3ce9ef90df7
Turger <4330948-Turger@users.noreply.gitlab.com>
2025-01-02 08:03:01 +0000

    refactor: wrap into namespace, add checks, change order for decrypt

--- src/util/Crypto.hpp
+++ src/util/Crypto.hpp
@@ -1,6 +1,10 @@
 #include <QByteArray>
 
+namespace chatterino {
+
 QString encryptMessage(QString &message, const QString &encryptionPassword);
 
 bool checkAndDecryptMessage(QString &message,
                             const QString &encryptionPassword);
+
+}  // namespace chatterino
